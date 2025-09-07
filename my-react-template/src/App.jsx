77858930b9d9c617231fd111ae6d428c11e0bbda
armanabramov <128611537+armanabramov@users.noreply.github.com>
2025-09-07 19:33:35 +0300

    feat: implement App.jsx with JSX and current year

--- my-react-template/src/App.jsx
+++ my-react-template/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+		{/* Декларативный рендер начинается */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+			<p>{new Date().getFullYear()}</p>
+			{/* Декларативный рендер заканчивается */}
     </>
   )
 }
